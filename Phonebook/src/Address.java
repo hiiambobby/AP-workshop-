7891442433647b026edcc84c2f7380cb758ed235
Saba St <129901353+hiiambobby@users.noreply.github.com>
2024-04-06 14:15:04 +0330

    Add files via upload

---
+++ Phonebook/src/Address.java
@@ -0,0 +1,13 @@
+public class Address {
+    private String zipCode;
+    private String country;
+    private String city;
+
+    public Address(String zipCode,String country,String city)
+    {
+        this.city = city;
+        this.zipCode=zipCode;
+        this.country = country;
+    }
+
+}
