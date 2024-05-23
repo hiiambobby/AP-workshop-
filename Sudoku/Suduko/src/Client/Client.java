1f229fd4f1c2439723aae754026a42aa7f4026fc
hiiiambobby <saba3st@gmail.com>
2024-05-23 21:43:42 +0330

    fixing the client part

--- Sudoku/Suduko/src/Client/Client.java
+++ Sudoku/Suduko/src/Client/Client.java
@@ -2,60 +2,77 @@ import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
-import com.sun.beans.decoder.StringElementHandler;
-
 public class Client {
-
     public static void main(String[] args) {
         Socket socket = null;
-        OutputStreamWriter osw = null;
-        InputStreamReader isr = null;
-        Scanner scanner = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferReader = null;
+        BufferedWriter bufferedWriter = null;
+
         try {
-            socket = new Socket("localhost", 12345);
-            isr = new InputStreamReader(socket.getInputStream());
-            osw = new OutputStreamWriter(socket.getOutputStream());
-            scanner = new Scanner(System.in);
+            socket = new Socket("LocalHost", 1234);
+            inputStreamReader = new InputStreamReader(socket.getInputStream());
+            outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+            bufferReader = new BufferedReader(inputStreamReader);
+            bufferedWriter = new BufferedWriter(outputStreamWriter);
+            Scanner scanner = new Scanner(System.in);
 
-            System.out.println("Enter input file name: \"inputname.txt\"");
-            String inputFileName = scanner.nextLine();
-            System.out.println("Enter output file name: \"outputname.txt\"");
-            String outputFileName = scanner.nextLine();
+            while (true) {
 
-            int[][] grid = new int[9][9];
-            try (Scanner fileScanner = new Scanner(new FileReader(inputFileName))) {
-                for (int i = 0; i < 9; i++) {
-                    for (int j = 0; j < 9; j++) {
-                        if (fileScanner.hasNextInt()) {
-                            grid[i][j] = fileScanner.nextInt();
-                        }
-                    }
-                }
-            } catch (FileNotFoundException e) {
-                System.out.println("Input file not found!");
-                return;
-            }
-
-            osw.writeObject(grid);
-            //int[][] solvedGrid = (int[][]) 
-
-            if (solvedGrid != null) {
-                try (FileWriter writer = new FileWriter(outputFileName)) {
+                System.out.println("Enter input file name: \"inputname.txt\"");
+                String inputFileName = scanner.nextLine();
+                System.out.println("Enter output file name: \"outputname.txt\"");
+                String outputFileName = scanner.nextLine();
+                int[][] grid = new int[9][9];
+                try (Scanner fileScanner = new Scanner(new FileReader(inputFileName))) {
                     for (int i = 0; i < 9; i++) {
                         for (int j = 0; j < 9; j++) {
-                            writer.write(solvedGrid[i][j] + " ");
+                            if (fileScanner.hasNextInt()) {
+                                grid[i][j] = fileScanner.nextInt();
+                            }
                         }
-                        writer.write("\n");
                     }
-                } catch (IOException e) {
-                    System.out.println("Error writing to output file!");
+                } catch (FileNotFoundException e) {
+                    System.out.println("Input file not found!");
+                    return;
                 }
-            } else {
-                System.out.println("No solution exists for the provided Sudoku puzzle.");
-            }
 
-        } catch (IOException | ClassNotFoundException e) {
+                bufferedWriter.write(message);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+
+                System.out.println("System: " + bufferReader.readLine());
+
+                if (message.equalsIgnoreCase("fuck")) {
+                    break;
+                }
+
+            }
+        } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (socket != null)
+                    socket.close();
+
+                if (inputStreamReader != null)
+                    inputStreamReader.close();
+
+                if (outputStreamWriter != null)
+                    outputStreamWriter.close();
+
+                if (bufferReader != null)
+                    bufferReader.close();
+
+                if (bufferedWriter != null)
+                    bufferedWriter.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
         }
+
     }
 }
