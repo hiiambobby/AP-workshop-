0d367579dc0817f9cdb9bc72c653603aea2305ed
hiiiambobby <saba3st@gmail.com>
2024-06-11 19:38:02 +0330

    the problem with start bottom fixed

--- Sudoku/Suduko/src/Client/MenuController.java
+++ Sudoku/Suduko/src/Client/MenuController.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -29,14 +30,16 @@ public class MenuController {
     }
 
     @FXML
-    void startSelected(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("FXML/Difficulty.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
-      //  scene.getStylesheets().add(getClass().getResource("FXML/Style.css").toExternalForm()); // Add this line
-        Stage primaryStage = (Stage) startButton.getScene().getWindow();
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    void startSelected(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("FXML/Difficulty.fxml"));
+            Scene scene = new Scene(root);
+            Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
